51112ab794f2cdb1e4296838ecd709a2f49b6e88
Bai <2818506404@qq.com>
2025-03-18 21:57:22 +0800

    docs: 更新 VitePress 主题配置并添加 base 路径
    - 在 theme/index.js 中添加 base 路径 "/docs-demo/"
    - 优化代码格式，统一使用双引号和逗号风格
    - 调整 Layout 组件的返回结构

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,17 +1,18 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import DefaultTheme from 'vitepress/theme'
-import './style.css'
+import { h } from "vue";
+import DefaultTheme from "vitepress/theme";
+import "./style.css";
 
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
+  base: "/docs-demo/",
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
+    });
   },
   enhanceApp({ app, router, siteData }) {
     // ...
-  }
-}
+  },
+};
