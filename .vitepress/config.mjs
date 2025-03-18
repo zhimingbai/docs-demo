f38eb30867ea31715a7cedc93297b1194fbc40fa
Bai <2818506404@qq.com>
2025-03-18 20:14:16 +0800

    第一次demo

---
+++ .vitepress/config.mjs
@@ -0,0 +1,73 @@
+import { defineConfig } from "vitepress";
+import { set_sidebar } from "./utils/auto_sidebar";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  head: [["link", { rel: "icon", href: "/logo.png" }]],
+  title: "我的博客",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: "/logo.png",
+
+    outline: {
+      label: "目录",
+      level: [2, 6],
+    },
+
+    nav: [
+      // { text: "Home", link: "/" },
+      {
+        text: "前端",
+        items: [{ text: "css", link: "/poem/css" }],
+      },
+      {
+        text: "408",
+        items: [{ text: "数据结构", link: "408/data-structure" }],
+      },
+      { text: "其他的东西", link: "/else" },
+    ],
+
+    // sidebar: [
+    //   {
+    //     text: "about",
+    //     items: [
+    //       { text: "个人介绍", link: "/about" },
+    //       { text: "Runtime API Examples", link: "/api-examples" },
+    //     ],
+    //   },
+    // ],
+
+    // sidebar: { "/about": set_sidebar("about") },
+
+    sidebar: false,
+    aside: "left",
+
+    socialLinks: [{ icon: "github", link: "https://github.com/zhimingbai" }],
+
+    footer: {
+      copyright: "Copyright © 2025 白芷茗",
+    },
+
+    // 设置搜索框的样式
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    },
+  },
+});
---
+++ index.md
@@ -0,0 +1,27 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "白芷茗"
+  text: "的个人博客"
+  tagline: 我是学习两年半的前端萌新，Ciallo～(∠・ω< )⌒★
+  image:
+    src: /background.png
+  actions:
+    - theme: brand
+      text: 个人介绍
+      link: /about
+    - theme: alt
+      text: markdown语法测试文件
+      link: /markdown-test
+
+features:
+  - title: Feature A
+    details: 特征1的描述
+  - title: Feature B
+    details: 特征1的描述
+  - title: Feature C
+    details: 特征1的描述
+---
+
